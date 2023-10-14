b3a2477b7bb98d96856f0b0485c248a1e5e87f1c
vivi-ana <acostaviviana.a@gmail.com>
2023-10-13 23:00:45 -0300

    Adding logout

---
+++ TierraMediaWeb/Pages/Account/Logout.cshtml.cs
@@ -0,0 +1,18 @@
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace TierraMediaWeb.Pages.Account
+{
+    public class LogoutModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+        public async Task<IActionResult> OnPostAsync()
+        {
+            await HttpContext.SignOutAsync("MyCookieAuth");
+            return RedirectToPage("Login");
+        }
+    }
+}
